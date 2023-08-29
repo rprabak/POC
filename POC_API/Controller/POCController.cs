7b63c05b00f53755320596f4a9474428cd4a4b5f
rprabak <Ravi.Prabakaran1@exxonmobil.com>
2023-08-29 16:25:35 -0500

    Hook the FE with API

---
+++ POC_API/Controller/POCController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POC_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class POCController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Get the string";
+        }
+    }
+}
--- POC_API/Startup.cs
+++ POC_API/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +17,7 @@ namespace POC_API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,32 @@ namespace POC_API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
+
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
+
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = @"..\POC-FE";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+
+            });
+
         }
     }
 }
